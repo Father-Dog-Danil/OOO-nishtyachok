d7a67c868febdf6a9b552c3f0aba32e56db97dda
s1eepys <seregayuferovv@yandex.ru>
2026-09-11 12:43:25 +0500

    ну там добавили ээээ

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,9 @@ const app = express();
 const PORT = 5000;
 
 // Тут пишем эндпоинты
+app.use(function(_, _, next){
+    console.log("Middleware 1");
+});
 
 app.get('/api/data', (req, res) => {
 
@@ -16,9 +19,27 @@ app.get('/api/data', (req, res) => {
 
 });
 
+app.get('/users', (req, res) => {
+
+    res.json({ message: 'юзеры' });
+    
+});
+
+app.get('/users/posts', (req, res) => {
+
+    res.json({ message: 'посты' });
+    
+});
+app.get('/users/posts/delete', (req, res) => {
+
+    res.json({ message: 'удаленные посты' });
+
+});
+
 app.listen(PORT, () => {
 
     console.log(`Сервер запущен на порту ${PORT}`);
+    console.log(`http://localhost:${PORT}`);
 
 
 });
